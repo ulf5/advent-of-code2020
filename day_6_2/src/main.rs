ca2b1cb5982de1916f18a1796d399e9176045c71
ulf <ulfahammar@gmail.com>
2020-12-06 13:00:48 +0100

    learning some better ways to work with bits

--- day_6_2/src/main.rs
+++ day_6_2/src/main.rs
@@ -1,38 +1,19 @@
-#![feature(iterator_fold_self)]
-
-use bit_vec::BitVec;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("input.txt")?;
-    let reader = BufReader::new(file);
+    let mut reader = BufReader::new(file);
+    let mut input = String::new();
+    reader.read_to_string(&mut input)?;
 
-    let mut all: Vec<usize> = vec![];
-    let mut chunk: Vec<BitVec> = vec![];
-    for line in reader.lines() {
-        let line = line?;
-        if line.is_empty() {
-            let c: BitVec = chunk
-                .into_iter()
-                .fold_first(|mut a, b| {
-                    a.and(&b);
-                    a
-                })
-                .unwrap();
-            let p = c.iter().filter(|x| *x).count();
-            all.push(p);
-            chunk = vec![];
-        } else {
-            let person: BitVec = line
-                .chars()
-                .fold(BitVec::from_elem(26, false), |mut acc, x| {
-                    acc.set((x as usize % 32usize) - 1usize, true);
-                    acc
-                });
-            chunk.push(person);
-        }
-    }
-    dbg!(all.iter().sum::<usize>());
+    dbg!(input
+        .split("\n\n")
+        .map(|group| group
+            .split_whitespace()
+            .map(|x| x.bytes().fold(0u32, |x, b| x | 1 << (b - b'a')))
+            .fold(!0, |x, g| x & g)
+            .count_ones())
+        .sum::<u32>());
     Ok(())
 }
