8750011ce519d40c3c3ee69a63cbf09bea5e8abf
ulf <ulfahammar@gmail.com>
2021-12-01 15:47:06 +0100

    some changes based on others' ideas

--- 2021/01_1/src/main.rs
+++ 2021/01_1/src/main.rs
@@ -1,5 +1,3 @@
-use std::iter;
-
 fn main() {
     let mut times = 0;
     let depths = include_str!("../input.txt")
@@ -7,7 +5,7 @@ fn main() {
         .map(|x| x.parse::<u32>().unwrap());
     depths
         .clone()
-        .zip(depths.skip(1).chain(iter::repeat(0)))
+        .zip(depths.skip(1))
         .for_each(|(cur, next)| {
             if next > cur {
                 times += 1
--- 2021/01_2/src/main.rs
+++ 2021/01_2/src/main.rs
@@ -1,13 +1,15 @@
 fn main() {
     let mut times = 0;
-    let depths: Vec<u32>  = include_str!("../input.txt")
+    let depths = include_str!("../input.txt")
         .lines()
-        .map(|x| x.parse::<u32>().unwrap())
-        .collect();
-    for i in 0..depths.len() - 3 {
-        if depths[i+1..i+4].iter().sum::<u32>() > depths[i..i+3].iter().sum() {
-            times += 1;
-        }
-    }
+        .map(|x| x.parse::<u32>().unwrap());
+    depths
+        .clone()
+        .zip(depths.skip(3))
+        .for_each(|(cur, next)| {
+            if next > cur {
+                times += 1
+            }
+        });
     dbg!(times);
 }
