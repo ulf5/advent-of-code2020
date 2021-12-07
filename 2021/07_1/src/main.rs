6844978f17b54050e1f697e6805f1fc1bc5c36cb
ulf <ulfahammar@gmail.com>
2021-12-07 18:42:55 +0100

    day 7

---
+++ 2021/07_1/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let crabs: Vec<i32> = include_str!("../input.txt")
+        .lines()
+        .map(|l| l.split(',').map(|e| e.parse::<i32>().unwrap()))
+        .flatten()
+        .collect();
+    let max = crabs.iter().max().unwrap();
+
+    let mut last_sum = 0;
+    for i in 0..*max {
+        let sum = crabs.iter().map(|c| (c - i).abs()).sum::<i32>();
+        if i > 0 && sum > last_sum {
+            dbg!(last_sum);
+            break;
+        }
+        last_sum = sum;
+    }
+}
